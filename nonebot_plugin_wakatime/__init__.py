aab3537a11bbba1b907ccbe9836b1e8d886ace6d
KomoriDev <mute231010@gmail.com>
2024-07-30 16:44:42 +0800

    :bookmark: v0.1.4

--- nonebot_plugin_wakatime/__init__.py
+++ nonebot_plugin_wakatime/__init__.py
@@ -36,7 +36,7 @@ __plugin_meta__ = PluginMetadata(
         "unique_name": "WakaTime",
         "orm_version_location": migrations,
         "author": "Komorebi <mute231010@gmail.com>",
-        "version": "0.1.3",
+        "version": "0.1.4",
     },
 )
 
