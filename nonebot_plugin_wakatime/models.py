02111928ff5d79e0e1a54e162b3a27066f543b55
KomoriDev <mute231010@gmail.com>
2024-08-01 09:57:32 +0800

    :sparkles: 使用 plugin-user 跨平台绑定

--- nonebot_plugin_wakatime/models.py
+++ nonebot_plugin_wakatime/models.py
@@ -6,9 +6,7 @@ class User(Model):
 
     __tablename__ = "wakatime"
 
-    user_id: Mapped[str] = mapped_column(primary_key=True)
+    id: Mapped[str] = mapped_column(primary_key=True)
     """User ID"""
-    platform: Mapped[str]
-    """User Account Platform"""
     access_token: Mapped[str]
     """Wakatime Access Token"""
