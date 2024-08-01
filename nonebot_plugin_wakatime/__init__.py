aa7588c2d6c9c00f4f9ebf7c3f4ab7594a249d59
KomoriDev <mute231010@gmail.com>
2024-08-01 10:11:39 +0800

    :beers: 修改 supported_adapters

--- nonebot_plugin_wakatime/__init__.py
+++ nonebot_plugin_wakatime/__init__.py
@@ -32,7 +32,9 @@ __plugin_meta__ = PluginMetadata(
     type="application",
     config=Config,
     homepage="https://github.com/KomoriDev/nonebot-plugin-wakatime",
-    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
+    supported_adapters=inherit_supported_adapters(
+        "nonebot_plugin_alconna", "nonebot_plugin_user"
+    ),
     extra={
         "unique_name": "WakaTime",
         "orm_version_location": migrations,
