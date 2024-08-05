bb4d21f10b222ff7d9da08effa65f2acc36c5835
Azide <rukuy@qq.com>
2024-08-05 13:35:25 +0800

    :sparkles: 将一些启动时判定移动到bootstrap中

---
+++ nonebot_plugin_wakatime/boostrap.py
@@ -0,0 +1,35 @@
+from typing import TYPE_CHECKING, cast
+from nonebot import get_driver as get_nonebot_driver, logger
+from nonebot.drivers import ASGIMixin, HTTPClientMixin
+
+from .config import config as wakatime_config
+
+driver = get_nonebot_driver()
+
+mountable = True
+plugin_enable = True
+
+if not isinstance(driver, HTTPClientMixin):
+    plugin_enable = False
+    raise RuntimeError("Driver must be HTTPClientMixin")
+
+if not isinstance(driver, ASGIMixin):
+    mountable = False
+    logger.warning(
+        "当前 NoneBot Driver 并非 ASGIMixin，"
+        "无法挂载自动注册 wakatime code 的路由，需要手动绑定"
+    )
+
+client_id = wakatime_config.client_id
+client_secret = wakatime_config.client_secret
+redirect_uri = wakatime_config.redirect_uri
+
+if client_id == "" or client_secret == "" or redirect_uri == "":
+    plugin_enable = False
+    logger.error("缺失必要配置项，已禁用该插件")
+
+
+if TYPE_CHECKING:
+    class Driver(HTTPClientMixin, ASGIMixin): ...
+
+    driver = cast(Driver, driver)
