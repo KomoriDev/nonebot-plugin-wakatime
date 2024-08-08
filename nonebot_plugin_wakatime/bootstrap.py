1c8cd6af6e655ca6a3bc6282ed211462f9e3f014
Azide <rukuy@qq.com>
2024-08-08 21:38:36 +0800

    :recycle: 优化 bootstrap 逻辑

--- nonebot_plugin_wakatime/bootstrap.py
+++ nonebot_plugin_wakatime/bootstrap.py
@@ -1,5 +1,7 @@
 from typing import TYPE_CHECKING, cast
 
+from yarl import URL
+
 from nonebot import logger
 from nonebot import get_driver as get_nonebot_driver
 from nonebot.drivers import ASGIMixin, HTTPClientMixin
@@ -8,19 +10,8 @@ from .config import config as wakatime_config
 
 driver = get_nonebot_driver()
 
-mountable = True
 plugin_enable = True
-
-if not isinstance(driver, HTTPClientMixin):
-    plugin_enable = False
-    raise RuntimeError("Driver must be HTTPClientMixin")
-
-if not isinstance(driver, ASGIMixin):
-    mountable = False
-    logger.warning(
-        "当前 NoneBot Driver 并非 ASGIMixin，"
-        "无法挂载自动注册 wakatime code 的路由，需要手动绑定"
-    )
+mountable = True
 
 client_id = wakatime_config.client_id
 client_secret = wakatime_config.client_secret
@@ -28,7 +19,29 @@ redirect_uri = wakatime_config.redirect_uri
 
 if not client_id or not client_secret or not redirect_uri:
     plugin_enable = False
+    mountable = False
     logger.error("缺失必要配置项，已禁用该插件")
+
+elif not isinstance(driver, HTTPClientMixin):
+    plugin_enable = False
+    mountable = False
+    raise RuntimeError("Driver must be HTTPClientMixin")
+
+elif not isinstance(driver, ASGIMixin):
+    mountable = False
+    logger.warning(
+        "当前 NoneBot Driver 并非 ASGIMixin，"
+        "无法挂载自动注册 wakatime code 的路由，需要手动绑定"
+    )
+
+elif not redirect_uri or not (
+    (host := URL(redirect_uri).host) and (host.endswith("wakatime.com"))
+):
+    mountable = False
+    logger.warning(
+        "redirect_uri 未设置或是 wakatime.com 域名，"
+        "无法挂载自动注册 wakatime code 的路由，需要手动绑定"
+    )
 
 
 if TYPE_CHECKING:
